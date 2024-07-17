3ee55b37938860434bd5ef21ed5fd3f0b2393447
Omri.Achituv <omrisb@gmail.com>
2024-07-17 17:26:43 +0300

    Remove Duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,17 +1,15 @@
-import React from "react";
 import { Game } from "../hooks/useGames";
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageURL from "../services/image-url";
-
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden" width="300px">
+    <Card>
       <Image src={getCroppedImageURL(game.background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
